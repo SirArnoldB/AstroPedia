fd5978a91dbd961c730e0fc72dfcc3db6d8f7962
Arnold Bhebhe <legendaryarnoldb@gmail.com>
2024-02-04 11:42:01 -0600

    add ar images

--- frontend/src/views/PlanetArView.jsx
+++ frontend/src/views/PlanetArView.jsx
@@ -1,45 +1,55 @@
 import { Container, Typography } from "@mui/material";
 import { PlanetAr } from "../components/planet-ar/PlanetAr";
 
-const pexel = (id) =>
-  `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`;
 const images = [
   // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
+  {
+    position: [0, 0, 1.5],
+    rotation: [0, 0, 0],
+    url: "https://images-assets.nasa.gov/image/PIA02006/PIA02006~orig.jpg",
+  },
   // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+  {
+    position: [-0.8, 0, -0.6],
+    rotation: [0, 0, 0],
+    url: "https://images-assets.nasa.gov/image/PIA10969/PIA10969~orig.jpg",
+  },
+  {
+    position: [0.8, 0, -0.6],
+    rotation: [0, 0, 0],
+    url: "https://images-assets.nasa.gov/image/PIA10119/PIA10119~orig.jpg",
+  },
   // Left
   {
     position: [-1.75, 0, 0.25],
     rotation: [0, Math.PI / 2.5, 0],
-    url: pexel(327482),
+    url: "https://images-assets.nasa.gov/image/PIA11376/PIA11376~orig.jpg",
   },
   {
     position: [-2.15, 0, 1.5],
     rotation: [0, Math.PI / 2.5, 0],
-    url: pexel(325185),
+    url: "https://images-assets.nasa.gov/image/EC05-0201-08/EC05-0201-08~orig.jpg",
   },
   {
     position: [-2, 0, 2.75],
     rotation: [0, Math.PI / 2.5, 0],
-    url: pexel(358574),
+    url: "https://images-assets.nasa.gov/image/PIA22357/PIA22357~orig.jpg",
   },
   // Right
   {
     position: [1.75, 0, 0.25],
     rotation: [0, -Math.PI / 2.5, 0],
-    url: pexel(227675),
+    url: "https://images-assets.nasa.gov/image/PIA03604/PIA03604~orig.jpg",
   },
   {
     position: [2.15, 0, 1.5],
     rotation: [0, -Math.PI / 2.5, 0],
-    url: pexel(911738),
+    url: "https://images-assets.nasa.gov/image/PIA08003/PIA08003~orig.jpg",
   },
   {
     position: [2, 0, 2.75],
     rotation: [0, -Math.PI / 2.5, 0],
-    url: pexel(1738986),
+    url: "https://images-assets.nasa.gov/image/PIA09967/PIA09967~orig.jpg",
   },
 ];
 
